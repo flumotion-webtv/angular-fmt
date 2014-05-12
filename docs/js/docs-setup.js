b15b556c8929296ebe3d4e20e662cbffb0cf2e24
Daniel M <dmana@flumotion.com>
2014-05-12 16:22:58 +0200

    Built angular-fmt from commit c174a5d on branch dash_mse

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,80 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "fmt.directive:fmtAddthis",
+      "shortName": "fmtAddthis",
+      "type": "directive",
+      "moduleName": "fmt",
+      "shortDescription": "AddThis toolbox wrapper that automatically gets synchronized with the pod selected by other Flumotion widgets like playlist directive.",
+      "keywords": "$scope addthis addthis-description addthis-title addthisdescription addthistitle api automatically buttons case checkbox class comma-separated ctrl default description directive directory example facebook false flumotion fmt fmt-addthis form-inline function google_plusone_share html http info input-block-level js large linkedin manage min module ng-controller ng-disabled ng-model num-preferred number numpreferred options owned playlist pod preferred pubid ra-52f8a2223eb7c112 script selected service services size small string synchronized text title toolbox true tuenti twitter type url use-preferred usepreferred var versus widgets wrapper"
+    },
+    {
+      "section": "api",
+      "id": "fmt.directive:fmtPlayer",
+      "shortName": "fmtPlayer",
+      "type": "directive",
+      "moduleName": "fmt",
+      "shortDescription": "HTML element that shows a player.",
+      "keywords": "add agent analytics api audio auto-play automatically autoplayenabled browser content css current de default defines directive ea element enables flash flumotion fmt fmt-player fmt-w force google hashbang height html html5 html5_flash html5_flash_dashjs http image module mp4 options order player player-flash player-html5 playerflash playerhtml5 pod poster posterurl reproduce select send seoenabled set src statistics statsenabled stream strobe strobe16 style supported tech-order technologies technology techorder true url video videojs vorwaerts-gmbh"
+    },
+    {
+      "section": "api",
+      "id": "fmt.directive:fmtPlayerDash",
+      "shortName": "fmtPlayerDash",
+      "type": "directive",
+      "moduleName": "fmt",
+      "shortDescription": "Player compatible with DASH.",
+      "keywords": "api commondatastorage compatible content dash directive fmt fmt-player-dash googleapis html http module mpd player url"
+    },
+    {
+      "section": "api",
+      "id": "fmt.directive:fmtPlaylist",
+      "shortName": "fmtPlaylist",
+      "type": "directive",
+      "moduleName": "fmt",
+      "shortDescription": "HTML element that shows a list of items and synchronize them with the player directive.",
+      "keywords": "$location $scope api auto-advance auto-advance-enabled autoadvanceenabled checkbox class console content creation css ctrl current-item-id currentitemid date-visible datevisible default directive disabled duration duration-visible durationvisible element enables event fixtures flumotion fmt fmt-playlist fmt-w fmtpodfinished function height hidden html input-block-level item items js json likes likes-visible likesvisible list load log min module name-visible namevisible ng-controller ng-model ng-show number player playlist receives requires script select selected selecting skip style synchronize text thumb thumb-visible thumbnail thumbvisible true type url var visible"
+    },
+    {
+      "section": "api",
+      "id": "fmt.filter:duration",
+      "shortName": "duration",
+      "type": "filter",
+      "moduleName": "fmt",
+      "shortDescription": "Transforms seconds to string &#39;HH:MM:SS&#39;",
+      "keywords": "api filter fmt hh html module ng-model number seconds string transforms type"
+    },
+    {
+      "section": "api",
+      "id": "fmt.filter:extension",
+      "shortName": "extension",
+      "type": "filter",
+      "moduleName": "fmt",
+      "shortDescription": "Returns the extension of a file in a URL",
+      "keywords": "api class extension file filter fmt html input-block-level module ng-model returns type url"
+    },
+    {
+      "section": "api",
+      "id": "fmt.filter:parse",
+      "shortName": "parse",
+      "type": "filter",
+      "moduleName": "fmt",
+      "shortDescription": "Parses a string into boolean, number or JSON object",
+      "keywords": "$filter $scope api boolean class ctrl filter fmt function html input-block-level js json module ng-controller ng-model number object parse parses return script string text type typeof var"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "jquery.min.js",
+    "angular.min.js",
+    "fmt-tpls.min.js"
+  ]
+};
